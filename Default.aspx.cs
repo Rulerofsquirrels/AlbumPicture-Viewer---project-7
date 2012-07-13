0a32c78afc6d3d226fa4b22a5b66a7e85967cd66
John C. <jcamacho@mail.usf.edu>
2012-07-13 19:57:15 -0400

    javascript update

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,6 +8,15 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        image1.Attributes.Add("onClick", "reimage();");
+        image2.Attributes.Add("onClick", "reimage();");
+        image3.Attributes.Add("onClick", "reimage();");
+        image4.Attributes.Add("onClick", "reimage();");
+        image5.Attributes.Add("onClick", "reimage();");
+    }
 
+    protected void image1_Click(object sender, ImageClickEventArgs e)
+    {
+   
     }
 }
--- JScript.js
+++ JScript.js
@@ -1,5 +1,4 @@
-﻿function reimage(string id)
-{
-    var imagedpic=document.getElementById(id);
-    alert("This would change the image");
+﻿function reimage()
+{    
+alert("Hi");
 }
